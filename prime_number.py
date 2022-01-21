bc201fb6e52179589c4372a9c6d3beba3f5628ea
Your Name <ubaydullayev@samtuit.uz>
2022-01-21 22:33:08 +0500

    3-d task

--- prime_number.py
+++ prime_number.py
@@ -9,5 +9,14 @@ def prime_number(n:int)->int:
     Returns:
         int: Number of prime numbers
     """
-
-    return 
+    lst = []
+    k = 0
+    for i in range (2, n+1):
+        for j in range (2, i):
+            if i % j == 0:
+                k += 1
+        if k == 0:
+            lst.append(i)
+        else:
+            k = 0
+    return len(lst)
