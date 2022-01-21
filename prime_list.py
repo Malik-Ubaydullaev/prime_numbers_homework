6dd9e90fd44eb7d4dabe919f26879f110e351031
Your Name <ubaydullayev@samtuit.uz>
2022-01-21 22:28:17 +0500

    2-nd task

--- prime_list.py
+++ prime_list.py
@@ -8,6 +8,15 @@ def prime_list(n:int)->list:
     Returns:
         list: List of prime numbers
     """
- 
-    return 
+    lst = []
+    k = 0
+    for i in range (2, n+1):
+        for j in range (2, i):
+            if i % j == 0:
+                k += 1
+        if k == 0:
+            lst.append(i)
+        else:
+            k = 0
+    return lst
 
