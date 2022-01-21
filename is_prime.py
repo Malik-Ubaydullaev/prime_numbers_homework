ef5bd155acc4383b8ccd95210642fe60bfd67d1a
Your Name <ubaydullayev@samtuit.uz>
2022-01-21 22:17:49 +0500

    1-st task

--- is_prime.py
+++ is_prime.py
@@ -8,7 +8,9 @@ def is_prime(n:int)->bool:
         bool: True if prime, False if not.
     """
     if n == 1:
-        return True
+        return False
+    if n == 0:
+        return False
     for  i in range(2, n):
         if n % i == 0:
             return False
